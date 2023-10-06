d8718d2d46467b2143659b901a7059a389794e4e
Lucemans <luc@lucemans.nl>
2023-10-06 23:53:33 +0000

    Update Padding

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -95,8 +95,8 @@ export default async function ({
     ]);
 
     return (
-        <div className="w-full flex flex-col gap-8 mt-4 lg:mt-10 px-6 py-8">
-            <div className="w-full max-w-md mx-auto">
+        <div className="w-full mt-4 lg:mt-10 px-6 py-8">
+            <div className="w-full max-w-md mx-auto flex flex-col gap-8">
                 <div className="text-center px-4">
                     <img
                         src="/frensday_2.png"
