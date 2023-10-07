9099db9b76bcc76fa8b7aa9ede6eaa8783298a93
Lucemans <luc@lucemans.nl>
2023-10-07 15:06:11 +0000

    Bump

--- components/POAPModal/POAPModal.tsx
+++ components/POAPModal/POAPModal.tsx
@@ -42,23 +42,25 @@ export const POAPModal: FC<{
     return (
         <div className="fixed bottom-0 inset-x-0 px-2 pb-4">
             <div className="w-full max-w-md3 mx-auto">
-                <div className="p-4 text-center relative flex flex-col items-center">
+                <div className="p-6 gap-4 text-center relative flex flex-col items-center">
                     <Creeper />
-                    <div className="absolute inset-x-0 bottom-0 top-12 bg-[#14032C] rounded-3xl -z-10"></div>
-                    <div className="w-24 h-24 bg-slate-100 rounded-full">
-                        <img
-                            src={metadata.image_url}
-                            alt=""
-                            className="w-full h-full object-cover"
-                        />
-                    </div>
-                    <div className="">
-                        <div className="px-4 py-2 btn w-fit mx-auto -translate-y-3 font-bold">
-                            Mint POAP
+                    <div className="absolute inset-x-0 bottom-0 top-0 bg-[#14032C] rounded-3xl -z-10"></div>
+                    <div className="w-full h-8 z-10 relative flex items-center justify-center">
+                        <div className="w-24 h-24 bg-slate-100 rounded-full -translate-y-14">
+                            <img
+                                src={metadata.image_url}
+                                alt=""
+                                className="w-24 h-24 object-cover"
+                            />
+                        </div>
+                        <div className="z-10 absolute bottom-0 w-full">
+                            <div className="px-6 py-2 btn w-fit mx-auto font-bold text-sm">
+                                Mint POAP
+                            </div>
                         </div>
                     </div>
                     <button
-                        className="absolute right-4 top-16 text-xl opacity-50"
+                        className="absolute right-4 text-xl opacity-50"
                         onClick={() => {
                             setDismissed(true);
                         }}
