c56e3e88bbea2974465122889383ae7cfb9981e5
Lucemans <luc@lucemans.nl>
2023-10-07 02:53:49 +0000

    Text Wrapping

--- components/XMTP/section.tsx
+++ components/XMTP/section.tsx
@@ -8,9 +8,10 @@ export const XMTPSection: FC<{ name: string; address: string }> = ({
 }) => {
     return (
         <div className="text-center space-y-4 mt-4">
-            <div className="text-2xl font-bold px-8">
-                Send {name} a <span className="text-[#3889FF]">frENSday</span>{' '}
-                message
+            <div className="text-3xl font-bold balance px-12">
+                Send {name}
+                <wbr /> a <wbr />
+                <span className="text-[#3889FF]">frENSday</span> message
             </div>
             <div className="flex flex-col gap-2">
                 <AButton
