b89a62b2d27a19eb5c058763153e7308e833ee0f
Luc <luc@lucemans.nl>
2024-02-18 20:01:45 +0000

    Bump

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,11 @@ export default function RootLayout({
         <html lang="en">
             <body>{children}</body>
             <script src="https://v3x.report/please.js" async={true} />
+            <script
+                defer
+                data-domain="ens.page"
+                src="https://ens.v3x.report/js/script.js"
+            ></script>
             {/* <body className="bg-[#2A2244] text-white">{children}</body> */}
         </html>
     );
