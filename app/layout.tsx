f41f5d895006fb4331cde018d8b0b8529930778a
Jonatan Holmgren <jonatan@jontes.page>
2024-02-27 21:28:02 +0100

    Correct script url

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
                 <script
                     defer
                     data-domain="ens.page"
-                    src="https://science.nt3.me/js/script.local.manual.js"
+                    src="https://ens.v3x.report/js/script.manual.js"
                 ></script>
                 <script
                     dangerouslySetInnerHTML={{
