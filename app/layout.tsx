26d9b464c2f03bf77ee0bedbebf5fbd8690e9aeb
Jonatan Holmgren <jonatan@jontes.page>
2024-02-29 19:43:34 +0100

    Bump

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
                 <script
                     defer
                     data-domain="ens.page"
-                    src="https://science.nt3.me/js/script.local.js"
+                    src="https://ens.v3x.report/js/script.local.js"
                 ></script>
                 <script
                     dangerouslySetInnerHTML={{
