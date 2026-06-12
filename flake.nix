f9c18c1b95f509ae61671fc070c6f4090c0f1051
Luc <luc@lucemans.nl>
2026-06-12 16:28:46 +0200

    Setup flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "koi devshell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          just
+          nodejs_24
+          pnpm
+        ];
+
+        shellHook = ''
+          export pnpm_config_auto_install_peers=false
+          export pnpm_config_ignore_scripts=true
+          just
+        '';
+      };
+    });
+}
